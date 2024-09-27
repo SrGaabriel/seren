2d083eccb2486a655ace91ffb1c8f2bdd33073bf
SrGaabriel <gaabrielms@proton.me>
2024-09-26 23:34:48 -0300

    tests(tianlong): add operation

--- tianlong/src/test/scala/Transcribing.scala
+++ tianlong/src/test/scala/Transcribing.scala
@@ -1,8 +1,9 @@
 package me.gabriel.tianlong
 
-import struct.DragonType
+import struct.{ConstantReference, DragonType}
 import transcriber.TianlongIrTranscriber
 
+import me.gabriel.tianlong.struct.DragonType.Int32
 import org.scalatest.*
 import org.scalatest.flatspec.AnyFlatSpec
 
@@ -14,6 +15,17 @@ class Transcribing extends AnyFlatSpec {
     val transcriber = new TianlongIrTranscriber()
     val result = transcriber.transcribe(module)
     assertContentEquals("define i8 @main(i8 %0, i8 %1) {}", result)
+  }
+
+  "An add operation" should "be properly transcribed" in {
+    val module = new TianlongModule()
+    val function = module.createFunction("main", List(DragonType.Int8, DragonType.Int8), DragonType.Int8)
+    function.assign(function.add(ConstantReference.Number("2", Int32), ConstantReference.Number("1", Int32)))
+    val transcriber = new TianlongIrTranscriber()
+    val result = transcriber.transcribe(module)
+    assertContentEquals("define i8 @main(i8 %0, i8 %1) {" +
+      "%2 = add i32 2, 1" +
+      "}", result)
   }
 
   private def assertContentEquals(expected: String, actual: String): Unit = {
